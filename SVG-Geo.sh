221da94b23b56aae957b35b206b363a7c4e45829
Alex Dufresne-Nappert <alex.dufresne-nappert@lumentum.com>
2024-01-19 13:28:57 -0500

    Moved files around
    - Moved files around

--- SVG-Geo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the path to the Python script
-PYTHON_SCRIPT_PATH="Python/SVG-Geometry.py"
-
-# Run the Python script
-python "$PYTHON_SCRIPT_PATH"
-
-read -p "Press Enter to close the window..."
